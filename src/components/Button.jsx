c8cfa456cc8bca65b2e1edc4e2097408ae669d28
Carlos Andre <contato.carlosdede@gmail.com>
2026-01-04 23:50:14 -0300

    feat: add initial task and Button components

---
+++ src/components/Button.jsx
@@ -0,0 +1,19 @@
+const Button = ({ children, variant }) => {
+  const getVariantClasses = () => {
+    if (variant === "primary") {
+      return "bg-[#00ADB5] text-white";
+    }
+    if (variant === "ghost") {
+      return "br-transparent text-[#818181]";
+    }
+  };
+  return (
+    <button
+      className={`flex items-center gap-1 rounded-md px-3 py-1 text-xs font-semibold transition hover:opacity-70 ${getVariantClasses()}`}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
