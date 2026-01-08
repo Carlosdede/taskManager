c79624647f27d79bb60880da5dc0461a88592ec7
Carlos <contato.carlosdede@gmail.com>
2026-01-08 18:20:10 -0300

    feat: add styles at Dialog component

--- src/components/AddTaskDialog.jsx
+++ src/components/AddTaskDialog.jsx
@@ -1,4 +1,6 @@
+import { Input } from "postcss";
 import { createPortal } from "react-dom";
+import Button from "./Button";
 
 const AddTaskDialog = ({ isOpen }) => {
   if (!isOpen) return null;
@@ -7,9 +9,18 @@ const AddTaskDialog = ({ isOpen }) => {
     <div className="fixed bottom-0 left-0 top-0 flex h-screen w-screen items-center justify-center backdrop-blur">
       <div className="rounded-xl bg-white p-5 text-center shadow">
         <h2 className="text-xl font-semibold text-[#35383E]">Nova Tarefa</h2>
-        <p className="mt-1 text-sm text-[#9A9C9F]">
+        <p className="my-1 mb-4 text-sm text-[#9A9C9F]">
           Insira as informações abaixo
         </p>
+        <div className="flex w-[336px] flex-col space-y-4">
+          <Input placeholder="Insira o título da tarefa" />
+          <Input placeholder="Horário" />
+          <Input placeholder="Descreva a tarefa" />
+          <div className="flex gap-3">
+            <Button variant>Cancelar</Button>
+            <Button variant>Cancelar</Button>
+          </div>
+        </div>
       </div>
     </div>,
     document.body
---
+++ src/components/Input.jsx
@@ -0,0 +1,9 @@
+const Input = ({ ...rest }) => {
+  return (
+    <input
+      className="rounded-lg border border-solid border-[#ECECEC] px-4 py-3"
+      {...rest}
+    />
+  );
+};
+export default Input;
