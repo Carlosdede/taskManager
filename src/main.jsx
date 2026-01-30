c02ba340922c2daf7266314b5dfbe71988242eb7
Carlos Andre <contato.carlosdede@gmail.com>
2026-01-30 00:13:51 -0300

    feat: add home and tasks page

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import TaskDetailsPage from "./pages/taskDetails.jsx";
 import { Toaster } from "sonner";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import TasksPage from "./pages/Tasks.jsx";
+
+import HomePage from "./pages/home.jsx";
 
 const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <HomePage />,
+  },
+  {
+    path: "/tasks",
+    element: <TasksPage />,
   },
   {
     path: "/task/:taskId",
